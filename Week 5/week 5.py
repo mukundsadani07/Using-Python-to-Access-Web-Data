a636980297d923460d482de7fafe24ff4e59a4ca
Mukund Sadani <54810884+mukundsadani07@users.noreply.github.com>
2020-05-30 12:59:33 +0530

    Add files via upload

---
+++ Week 5/week 5.py
@@ -0,0 +1,21 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+url= input("Enter location:")
+print("Retrieving",url)
+
+data = urllib.request.urlopen(url).read()
+print('Retrieved', len(data), 'characters')
+tree = ET.fromstring(data)
+count = 0
+total = 0
+#for comment in comments.findall('comment'):
+for comments in tree.findall('comments/comment'):
+    total += int(comments.find('count').text)
+    count = count + 1
+print("count:",count)
+print("Sum:",total)
+
+
+
